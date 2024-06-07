897f592aec1f6f67cef7c6911a4aa172d188212d
yopiangga <yopigambyok@gmail.com>
2024-06-08 00:21:20 +0700

    mvp

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ApplicationRecord
+    after_create_commit {broadcast_message}
+
+    private
+
+    def broadcast_message
+        ActionCable.server.broadcast('MessagesChannel', {
+            id:, body:
+        })
+    end
+end
